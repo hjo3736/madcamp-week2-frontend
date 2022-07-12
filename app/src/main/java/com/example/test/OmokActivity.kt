836a6360199b0a26c8cad0dc570d9895251028b3
leejy12 <leejy31415@gmail.com>
2022-07-12 15:59:51 +0900

    disconnect fix

--- app/src/main/java/com/example/test/OmokActivity.kt
+++ app/src/main/java/com/example/test/OmokActivity.kt
@@ -3,6 +3,7 @@ package com.example.test
 import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.res.Resources
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
@@ -16,6 +17,11 @@ import com.android.volley.Request
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.WebSocket
+import okhttp3.WebSocketListener
+import okio.ByteString
 import org.json.JSONObject
 import org.json.JSONTokener
 import java.lang.Exception
@@ -33,7 +39,7 @@ fun getPieceColor(player: Int): Int {
     throw Exception()
 }
 
-class OmokActivity : AppCompatActivity(), MessageListener {
+class OmokActivity : AppCompatActivity() {
     private val serverUrl = "ws://192.249.18.128/websockets"
 
     // game stuff
@@ -59,7 +65,125 @@ class OmokActivity : AppCompatActivity(), MessageListener {
     private lateinit var searchGameDlg: Dialog
     private lateinit var foundGameDialog: Dialog
 
-    fun placeMove(player: Int, row: Int, col: Int, ) {
+    // websocket stuff
+    private lateinit var client: OkHttpClient
+    private lateinit var ws: WebSocket
+
+    private inner class OmokWebSocketListener : WebSocketListener() {
+        override fun onOpen(webSocket: WebSocket, response: Response) {
+            super.onOpen(webSocket, response)
+        }
+
+        override fun onMessage(webSocket: WebSocket, text: String) {
+            val msg = JSONTokener(text).nextValue() as JSONObject
+
+            when (msg.getString("type")) {
+                "waiting" -> {
+                    runOnUiThread {
+                        searchGameDlg.show()
+                    }
+                }
+                "gameFound" -> {
+                    /*
+                    {
+                        "type": "gameFound",
+                        "gameId": string(uuid),
+                        "player": 1 or 2,
+                        "opponent": {
+                            "nickname": string,
+                            "elo_rating": int,
+                            "school": string
+                        }
+                    }
+                    */
+                    runOnUiThread {
+                        val opponent = msg.getJSONObject("opponent")
+                        val opponentNickname = opponent.getString("nickname")
+                        val opponentRating = opponent.getInt("elo_rating")
+                        val opponentSchool = opponent.getString("school")
+                        gameId = msg.getString("gameId")
+                        println(gameId)
+
+                        myTurn = msg.getInt("player") == 1
+
+                        // 선공
+                        if (myTurn) {
+                            player = 1
+                        }
+                        else {
+                            player = 2
+                        }
+
+                        findViewById<ImageView>(R.id.myColor).setImageResource(getPieceColor(player))
+                        findViewById<ImageView>(R.id.opponentColor).setImageResource(getPieceColor(3 - player))
+
+                        foundGameDialog.show()
+
+                        opponentNicknameTextView.text = opponentNickname
+                        opponentRatingTextView.text = opponentRating.toString()
+                        opponentSchoolImageView.setImageResource(SchoolInfo.getLogo(opponentSchool))
+                    }
+                }
+                "moveResult" -> {
+                    /*
+                    {
+                        "type": "moveResult",
+                        "moveResult": {
+                            "player": 1 or 2,
+                            "row": int,
+                            "col": int,
+                            "status": 0, 1, 2 or 3
+                        },
+                        "newRating": int
+                    }
+                    */
+                    runOnUiThread {
+                        val moveResult = msg.getJSONObject("moveResult")
+                        val lastPlayer = moveResult.getInt("player")
+                        val lastMoveRow = moveResult.getInt("row")
+                        val lastMoveCol = moveResult.getInt("col")
+                        val status = moveResult.getInt("status")
+
+                        if (status == 0) {
+                            board[lastMoveRow][lastMoveCol] = lastPlayer
+                            if (player != lastPlayer) {
+                                myTurn = true
+                            }
+
+                            placeMove(lastPlayer, lastMoveRow, lastMoveCol)
+                        }
+                        else {
+                            val dialog = Dialog(applicationContext)
+                            val newRating = msg.getInt("newRating")
+                            dialog.setContentView(R.layout.game_result_dialog)
+                            val winOrLose = dialog.findViewById<TextView>(R.id.winOrLose)
+
+                            if (status == 3) {
+                                winOrLose.text = "무승부"
+                            }
+                            else {
+                                winOrLose.text = if (status == player) "승리" else "패배"
+                            }
+
+                            dialog.findViewById<TextView>(R.id.myOldRating).text = myRating.toString()
+                            dialog.findViewById<TextView>(R.id.myNewRating).text = newRating.toString()
+                            dialog.findViewById<Button>(R.id.okButton).setOnClickListener {
+                                finish()
+                            }
+
+                            dialog.show()
+                        }
+                    }
+                }
+            }
+        }
+
+        override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
+            webSocket.close(1000, reason)
+        }
+    }
+
+    fun placeMove(player: Int, row: Int, col: Int) {
         val imageView = ImageView(this)
         imageView.setImageResource(getPieceColor(player))
         imageView.requestLayout()
@@ -78,12 +202,16 @@ class OmokActivity : AppCompatActivity(), MessageListener {
         }
 
         constraintLayout.addView(imageView)
+
+        val mediaPlayer = MediaPlayer.create(this, R.raw.sound_fin)
+        mediaPlayer.start()
     }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_omok)
+
 
         myNicknameTextView = findViewById(R.id.myNicknameTextView)
         myRatingTextView = findViewById(R.id.myRatingTextView)
@@ -93,7 +221,6 @@ class OmokActivity : AppCompatActivity(), MessageListener {
         opponentSchoolImageView = findViewById(R.id.opponentSchoolImageView)
 
         val email = intent.getStringExtra("email")
-//        val email = "leejy31415@gmail.com"
 
         omokBoardView = findViewById(R.id.omokBoard);
         concedeButton = findViewById(R.id.concedeButton);
@@ -111,12 +238,11 @@ class OmokActivity : AppCompatActivity(), MessageListener {
                         if (board[row][col] == 0) {
                             myTurn = false
                             val gson = Gson()
-                            WebSocketManager.sendMessage(gson.toJson(OmokMove(gameId, player, row, col)))
+//                            WebSocketManager.sendMessage(gson.toJson(OmokMove(gameId, player, row, col)))
+                            ws.send(gson.toJson(OmokMove(gameId, player, row, col)))
                         }
                     }
-                    else -> {
-
-                    }
+                    else -> { }
                 }
                 true
             }
@@ -132,7 +258,8 @@ class OmokActivity : AppCompatActivity(), MessageListener {
 
             concedeYesButton.setOnClickListener {
                 val gson = Gson()
-                WebSocketManager.sendMessage(gson.toJson(OmokMove(gameId, player, -1, -1)))
+//                WebSocketManager.sendMessage(gson.toJson(OmokMove(gameId, player, -1, -1)))
+                ws.send(gson.toJson(OmokMove(gameId, player, -1, -1)))
                 dialog.dismiss()
             }
 
@@ -160,8 +287,13 @@ class OmokActivity : AppCompatActivity(), MessageListener {
                     mySchoolImageView.setImageResource(SchoolInfo.getLogo(mySchool))
 
                     val wsConUrl = "$serverUrl?nickname=$myNickname&elo_rating=$myRating&school=$mySchool"
-                    WebSocketManager.init(wsConUrl, this)
-                    WebSocketManager.connect()
+//                    WebSocketManager.init(wsConUrl, this)
+//                    WebSocketManager.connect()
+
+                    client = OkHttpClient()
+                    val request = okhttp3.Request.Builder().url(wsConUrl).build()
+                    val listener = OmokWebSocketListener()
+                    ws = client.newWebSocket(request, listener)
                 }
             },
             {}
@@ -191,127 +323,10 @@ class OmokActivity : AppCompatActivity(), MessageListener {
         omokBoardView.layoutParams.height = boardWidth
     }
 
-//    override fun onBackPressed() {
-//        super.onBackPressed()
-//        WebSocketManager.close(myNickname)
-//    }
-
-    override fun onConnectSuccess() {}
-
-    override fun onConnectFailed() {}
-
-    override fun onClose() {
-        println("HERE!")
-    }
-
-    override fun onMessage(text: String?) {
-        val msg = JSONTokener(text).nextValue() as JSONObject
-
-        when (msg.getString("type")) {
-            "waiting" -> {
-                runOnUiThread {
-                    searchGameDlg.show()
-                }
-            }
-            "gameFound" -> {
-                /*
-                {
-                    "type": "gameFound",
-                    "gameId": string(uuid),
-                    "player": 1 or 2,
-                    "opponent": {
-                        "nickname": string,
-                        "elo_rating": int,
-                        "school": string
-                    }
-                }
-                */
-                runOnUiThread {
-                    val opponent = msg.getJSONObject("opponent")
-                    val opponentNickname = opponent.getString("nickname")
-                    val opponentRating = opponent.getInt("elo_rating")
-                    val opponentSchool = opponent.getString("school")
-                    gameId = msg.getString("gameId")
-                    println(gameId)
-
-                    myTurn = msg.getInt("player") == 1
-
-                    // 선공
-                    if (myTurn) {
-                        player = 1
-                    }
-                    else {
-                        player = 2
-                    }
-
-                    findViewById<ImageView>(R.id.myColor).setImageResource(getPieceColor(player))
-                    findViewById<ImageView>(R.id.opponentColor).setImageResource(getPieceColor(3 - player))
-
-                    foundGameDialog.show()
-
-                    opponentNicknameTextView.text = opponentNickname
-                    opponentRatingTextView.text = opponentRating.toString()
-                    opponentSchoolImageView.setImageResource(SchoolInfo.getLogo(opponentSchool))
-                }
-            }
-            "moveResult" -> {
-                /*
-                {
-                    "type": "moveResult",
-                    "moveResult": {
-                        "player": 1 or 2,
-                        "row": int,
-                        "col": int,
-                        "status": 0, 1, 2 or 3
-                    },
-                    "newRating": int
-                }
-                */
-                runOnUiThread {
-                    val moveResult = msg.getJSONObject("moveResult")
-                    val lastPlayer = moveResult.getInt("player")
-                    val lastMoveRow = moveResult.getInt("row")
-                    val lastMoveCol = moveResult.getInt("col")
-                    val status = moveResult.getInt("status")
-
-                    if (status == 0) {
-                        this.board[lastMoveRow][lastMoveCol] = lastPlayer
-                        if (this.player != lastPlayer) {
-                            myTurn = true
-                        }
-
-                        placeMove(lastPlayer, lastMoveRow, lastMoveCol)
-                    }
-                    else {
-                        val dialog = Dialog(this)
-                        val newRating = msg.getInt("newRating")
-                        dialog.setContentView(R.layout.game_result_dialog)
-                        val winOrLose = dialog.findViewById<TextView>(R.id.winOrLose)
-
-                        if (status == 3) {
-                            winOrLose.text = "무승부"
-                        }
-                        else {
-                            winOrLose.text = if (status == player) "승리" else "패배"
-                        }
-
-                        dialog.findViewById<TextView>(R.id.myOldRating).text = myRating.toString()
-                        dialog.findViewById<TextView>(R.id.myNewRating).text = newRating.toString()
-                        dialog.findViewById<Button>(R.id.okButton).setOnClickListener {
-                            finish()
-                        }
-
-                        dialog.show()
-                    }
-                }
-            }
-        }
-    }
-
     override fun onDestroy() {
         runOnUiThread {
-            super.onDestroy ()
-            WebSocketManager.close(myNickname)
+            super.onDestroy()
+            ws.close(1000, myNickname)
         }
     }
 }
